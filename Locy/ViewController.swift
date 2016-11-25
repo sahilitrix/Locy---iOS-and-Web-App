6c6d16ad0158946a76521942692ce720f1ff1315
Sahit Penmatcha <sahilitrix123@gmail.com>
2016-11-25 17:25:04 -0500

    First commit

--- Locy/ViewController.swift
+++ Locy/ViewController.swift
@@ -7,14 +7,102 @@
 //
 
 import UIKit
+import MapKit
+import CloudKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    
+    var lastKnownLocation:CLLocation?
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        
+    }
+    @IBAction func updateLocation(_ sender: Any) {
+        
+        let authCode = CLLocationManager.authorizationStatus()
+        
+        if authCode == CLAuthorizationStatus.notDetermined && locationManager.responds(to: #selector(CLLocationManager.requestWhenInUseAuthorization)) {
+            if Bundle.main.object(forInfoDictionaryKey: "NSLocationWhenInUseUsageDescription") != nil {
+                locationManager.requestWhenInUseAuthorization()
+            }else{
+                print("No description provided")
+            }
+        }else{
+            getLocation()
+        }
+        
+    }
+    
+    func getLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        
+        if let currentLocation = locations.first {
+            
+            lastKnownLocation = currentLocation
+            
+            let currentPosition = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+            let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            let region = MKCoordinateRegion(center: currentPosition, span: coordinateSpan)
+            
+            mapView.setRegion(region, animated: true)
+            
+            let positionPin = MKPointAnnotation()
+            positionPin.coordinate = currentPosition
+            positionPin.title = "Your position"
+            positionPin.subtitle = "What an awesome place"
+            
+            mapView.addAnnotation(positionPin)
+        }
     }
 
+    @IBAction func shareLocation(_ sender: Any) {
+        
+        if lastKnownLocation != nil {
+            let database = CKContainer.default().publicCloudDatabase
+            
+            let locationAlert = UIAlertController(title: "Your Location", message: "Enter a title for your shared location", preferredStyle: .alert)
+            locationAlert.addTextField(configurationHandler: { (textfield:UITextField) in
+                textfield.placeholder = "Your location"
+            })
+            
+            locationAlert.addAction(UIAlertAction(title: "Send", style: .default, handler: { (action:UIAlertAction) in
+                
+                if let textFieldContent = locationAlert.textFields?.first?.text, locationAlert.textFields?.first?.text != "" {
+                    let newLocation = CKRecord(recordType: "Position")
+                    newLocation["locationTitle"] = textFieldContent as CKRecordValue?
+                    newLocation["sharedLocation"] = self.lastKnownLocation!
+                    
+                    database.save(newLocation, completionHandler: { (record:CKRecord?, error:NSError?) in
+                        if error != nil {
+                            print(error?.localizedDescription)
+                        }
+                    } as! (CKRecord?, Error?) -> Void)
+                    
+                }
+                
+           
+            }))
+            
+            locationAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            
+            self.present(locationAlert, animated: true, completion: nil)
+            
+            
+        }
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
